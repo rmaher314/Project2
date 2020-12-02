569f3db31522508f87a383b552f119744123c750
Cory Potter <pottercory22@gmail.com>
2020-12-01 20:22:18 -0600

    Updated Flask App

--- app.py
+++ app.py
@@ -1,10 +1,39 @@
-# import Flask
-from flask import Flask
+import numpy as np
 
-# Create an app, being sure to pass __name__
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+
+#################################################
+# Database Setup
+#################################################
+engine = create_engine("sqlite:///ironman.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save reference to the table
+Ironman = Base.classes.ironman
+
+#################################################
+# Flask Setup
+#################################################
 app = Flask(__name__)
 
+
+#################################################
+# Flask Routes
+#################################################
 @app.route("/")
 def home():
     print('Server received request for Home page...')
-    return "Ironman Analysis"
+    return ("Ironman Analysis")
+
+if __name__ == '__main__':
+    app.run(debug=True)
