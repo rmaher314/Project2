1406fb1d88d18e04170712d4fb99078c2cb4502e
Cory Potter <pottercory22@gmail.com>
2020-12-04 18:27:28 -0600

    created generate_db.py and archived ipynb

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
-
+import pandas as pd
+from config import username, password
 from flask import Flask, jsonify, render_template
 
 
@@ -33,6 +34,11 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template('index.html')
+
+# this is how we send data to javascript
+@app.route("/api/ironman/getData")
+def race_stats():
+    return jsonify()
 
 if __name__ == '__main__':
     app.run(debug=True)
