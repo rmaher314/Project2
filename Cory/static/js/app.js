d9c812112a1a00a809b26f212f37a2ffab72cbf1
Cory Potter <pottercory22@gmail.com>
2020-12-08 07:19:42 -0600

    refactored app.py with pandas

--- Cory/static/js/app.js
+++ Cory/static/js/app.js
@@ -56,19 +56,19 @@ function updatePage() {
 
 }
 
-// // set the dimensions and margins of the graph
-// var margin = {top: 10, right: 30, bottom: 20, left: 50},
-//     width = 460 - margin.left - margin.right,
-//     height = 400 - margin.top - margin.bottom;
+// set the dimensions and margins of the graph
+var margin = {top: 10, right: 30, bottom: 20, left: 50},
+    width = 460 - margin.left - margin.right,
+    height = 400 - margin.top - margin.bottom;
 
-// // append the svg object to the body of the page
-// var svg = d3.select("#bar_chart")
-//   .append("svg")
-//     .attr("width", width + margin.left + margin.right)
-//     .attr("height", height + margin.top + margin.bottom)
-//   .append("g")
-//     .attr("transform",
-//           "translate(" + margin.left + "," + margin.top + ")");
+// append the svg object to the body of the page
+var svg = d3.select("#bar")
+  .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform",
+          "translate(" + margin.left + "," + margin.top + ")");
 
 d3.json("/api/bar_chart").then((data) => {  
     var keys = [];
