6b7bbfeceae5349cd30f02bcb7c93211ecd7ab84
Cory Potter <pottercory22@gmail.com>
2020-12-08 19:52:46 -0600

    Added my changes to the main files

--- data/generate_db.py
+++ data/generate_db.py
@@ -1,6 +1,8 @@
 import sqlalchemy
 from sqlalchemy import create_engine
 import pandas as pd
+import datetime as dt
+from datetime import time
 
 # Read in race stats csv and convert it to a dataframe
 csv_file = "2019_Ironman_World_Championship_Results.csv"
@@ -13,15 +15,17 @@ countries_df = pd.read_csv(csv_file)
 # merge both dataframes into one dataframe on country code
 merged_df = pd.merge(race_stats_df, countries_df, left_on="Country", right_on="Alpha_3_code")
 
+# Convert race times to total seconds
+merged_df['Swim'] = ((merged_df['Swim_h'] * 3600) + (merged_df['Swim_m'] * 60) + merged_df['Swim_s'])
+merged_df['Bike'] = ((merged_df['Bike_h'] * 3600) + (merged_df['Bike_m'] * 60) + merged_df['Bike_s'])
+merged_df['Run'] = ((merged_df['Run_h'] * 3600) + (merged_df['Run_m'] * 60) + merged_df['Run_s'])
+merged_df['Overall'] = ((merged_df['Overall_h'] * 3600) + (merged_df['Overall_m'] * 60) + merged_df['Overall_s'])
+merged_df['T1'] = ((merged_df['T1_h'] * 3600) + (merged_df['T1_m'] * 60) + merged_df['T1_s'])
+merged_df['T2'] = ((merged_df['T2_h'] * 3600) + (merged_df['T2_m'] * 60) + merged_df['T2_s'])
+
 # Remove unnecessary columns and rename columns
 ironman_df = merged_df.loc[:, ['BIB', 'Last_Name', 'First_Name', 'Country_x', 'Country_y', 'Gender', 'Division', 'Swim', 'Bike', 'Run', 'Overall', 'Division_Rank', 'Gender_Rank', 'Overall_Rank', 'T1', 'T2', 'Latitude_average', 'Longitude_average']].dropna()
 ironman_df = ironman_df.rename(columns={"Country_x":"Alpha_3_code", "Country_y":"Country"})
-
-# Convert strings to time values
-ironman_df.loc[:, 'Swim'] = pd.to_datetime(ironman_df.loc[:, 'Swim'], format='%H:%M:%S')
-ironman_df.loc[:, 'Bike'] = pd.to_datetime(ironman_df.loc[:, 'Bike'], format='%H:%M:%S')
-ironman_df.loc[:, 'Run'] = pd.to_datetime(ironman_df.loc[:, 'Run'], format='%H:%M:%S')
-ironman_df.loc[:, 'Overall'] = pd.to_datetime(ironman_df.loc[:, 'Overall'], format='%H:%M:%S')
 
 # save this to sqllite
 engine = create_engine("sqlite:///ironman.sqlite")
