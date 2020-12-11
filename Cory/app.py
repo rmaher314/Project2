7bfdd0d36303b0d900579d5114b23ec06853c553
Cory Potter <pottercory22@gmail.com>
2020-12-10 19:19:57 -0600

    created initial bar chart api

--- Cory/app.py
+++ Cory/app.py
@@ -36,16 +36,39 @@ def race_stats_api():
         Overall, Division_Rank, Gender_Rank, Overall_Rank, Latitude_average, Longitude_average\
             FROM race_stats Order by Division", conn).to_json(orient='records')
 
-# display with time on y or normalize to who comleted 
-# divide everything by the fastest time ever
 
+# need to update route to look for selectedItem in the dropdown
 # create route for bar chart data
-@app.route("/api/bar_chart/")
-def bar_chart_api():
+@app.route("/api/bar_chart/<selectedItem>")
+def bar_chart_api(selectedItem):
+    return f'SelectedItem: {selectedItem}'
+    # create gender variable for filtering
+    genderLetter = 'M'
+    if selectedItem.startswith('F'):
+        genderLetter = 'F'
     # connect to our database
     conn = engine.connect()
     # return query results
-    return pd.read_sql("select Division, avg(Swim) as Swim, avg(Bike) as Bike, avg(Run) as Run FROM race_stats GROUP BY Division ORDER BY Division", conn).to_json(orient='records')
+    return pd.read_sql("select Division, avg(Swim) as Swim, avg(Bike) as Bike, avg(Run) as Run FROM race_stats WHERE Division LIKE '%g' GROUP BY Division ORDER BY Division" %genderLetter, conn).to_json(orient='records')
+
+    # if selectedItem.startswith('F'):
+    #     # connect to our database
+    #     conn = engine.connect()
+    #     # return query results
+    #     return pd.read_sql("select Division, avg(Swim) as Swim, avg(Bike) as Bike, avg(Run) as Run FROM race_stats WHERE Division LIKE 'F%' GROUP BY Division ORDER BY Division", conn).to_json(orient='records')
+    # else:
+    #     # connect to our database
+    #     conn = engine.connect()
+    #     # return query results
+    #     return pd.read_sql("select Division, avg(Swim) as Swim, avg(Bike) as Bike, avg(Run) as Run FROM race_stats WHERE Division LIKE 'M%' GROUP BY Division ORDER BY Division", conn).to_json(orient='records')
+
+@app.route("/api/bar_chart/initial")
+def init_bar_chart():
+
+    # connect to our database
+    conn = engine.connect()
+    # return query results
+    return pd.read_sql("select Division, avg(Swim) as Swim, avg(Bike) as Bike, avg(Run) as Run FROM race_stats WHERE Division LIKE 'F%' GROUP BY Division ORDER BY Division", conn).to_json(orient='records')
 
 @app.route("/api/top_ten_table/<selectedItem>")
 def table_api(selectedItem):
